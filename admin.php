c9b468fc0d78f4c1192b5f71cd8928fe3eaf61c2
WAIO21 <132699818+WAIO21@users.noreply.github.com>
2023-09-15 16:25:54 -0300

    Add files via upload
    pagina principal de admin

---
+++ admin.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="./css/login.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+</head>
+<body>
+
+    <div class="container-fluid">
+
+        <h1>¡HOLA ADMINISTRADOR!</h1>
+        <img src="./img/logoh.png" alt="logoh">
+        <h2>Elija la operación que se desea realizar</h2>
+
+    
+
+        <div class="botones">
+            
+            <button type="button" class="btn btn-outline-danger" value="Acceder">REGISTRAR PACIENTE</button>
+            <button type="button" class="btn btn-outline-danger" value="Acceder">REGISTRAR REPORTE MÉDICO</button>
+            <button type="button" class="btn btn-outline-danger" value="Acceder">REGISTRAR MÉDICO/ENFERMERO</button>
+            <button type="button" class="btn btn-outline-danger" value="Acceder">REGISTRAR ÁREA</button>
+            <button type="button" class="btn btn-outline-danger" value="Acceder">REGISTROS DE LLAMADAS</button>
+
+        </div>
+        
+    </div>
+</body>
+</html>
