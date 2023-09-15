d93a17976f5d9c92d987ac4db4cbd602a47d77cc
WAIO21 <132699818+WAIO21@users.noreply.github.com>
2023-09-15 15:24:40 -0300

    Add files via upload

---
+++ area.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Area</title>
+</head>
+<body>
+    
+        <form action ="guardarArea.php" method="POST" enctype="multipart/form-data">
+
+            <input type="text" REQUIRED name="area" placeholder="Area" value=""/>
+            <input type="submit"value="aceptar"/>
+
+        </form>
+    
+</body>
+</html>
