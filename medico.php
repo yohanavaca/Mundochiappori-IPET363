6db28ba20112547c5bb5b58e200ce1244c74fb49
MileColmenares <132697364+MileColmenares@users.noreply.github.com>
2023-09-18 15:03:06 -0300

    HACER CSS

--- medico.php
+++ medico.php
@@ -1,35 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="./css/login.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-
+    <title>Consulta de Datos Médicos</title>
 </head>
 <body>
-
-    <div class="container-fluid">
-
-        <h1>¡BIENVENIDO!</h1>
-        <img src="./img/logoh.png" alt="logoh">
-        <h2>Elija la operación que se desea realizar</h2>
-
+    <link rel="stylesheet" href="css/style.css">
+    <h1>Ingrese su DNI para consultar sus datos médicos</h1>
+    <form action="consultaMedico.php" method="POST">
+        <label for="dni">DNI:</label>
+        <input type="text" name="dni" id="dni" required>
+        <input type="submit" value="Consultar">
+    </form>
     
-
-        <div class="botones">
-            <a href="reportes.php">
-             <button type="button" class="btn btn-outline-danger" value="Acceder">REPORTES</button>
-            </a>
-            <a href="datos_reportes.php">
-            <button type="button" class="btn btn-outline-danger" value="Acceder">PACIENTE REGISTRADOS</button>
-            </a>
-
-            
-            
-        </div>
-        
-    </div>
 </body>
 </html>
