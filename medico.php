6a2dbb377bc20a7c018304688f3c5acffd206d2c
yohanavaca <132697011+yohanavaca@users.noreply.github.com>
2023-09-18 14:34:05 -0300

    Add files via upload

---
+++ medico.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="./css/login.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+</head>
+<body>
+
+    <div class="container-fluid">
+
+        <h1>¡BIENVENIDO!</h1>
+        <img src="./img/logoh.png" alt="logoh">
+        <h2>Elija la operación que se desea realizar</h2>
+
+    
+
+        <div class="botones">
+            <a href="reportes.php">
+             <button type="button" class="btn btn-outline-danger" value="Acceder">REPORTES</button>
+            </a>
+            <a href="datos_reportes.php">
+            <button type="button" class="btn btn-outline-danger" value="Acceder">PACIENTE REGISTRADOS</button>
+            </a>
+
+            
+            
+        </div>
+        
+    </div>
+</body>
+</html>
