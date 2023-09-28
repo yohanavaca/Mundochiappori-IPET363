2b22d0b2b8efa0282014522fe47ea86eedd7df2e
WAIO21 <132699818+WAIO21@users.noreply.github.com>
2023-09-28 16:24:25 -0300

    Update consultaPaciente.php

--- olimpiadas-grupo8/consultaPaciente.php
+++ olimpiadas-grupo8/consultaPaciente.php
@@ -1,6 +1,8 @@
 <?php
     include("conexion.php");
 
+//traemos los datos desde el formulario
+
     $nombre = $_POST['nombre'];
     $apellido = $_POST['apellido'];
     $dni = $_POST['dni'];
@@ -15,18 +17,26 @@
     $fecha_nacimiento = $_POST['fecha_nacimiento'];
     $medicoAsignado= $_POST['medicoAsignado'];
 
+//en el caso que no se ingrese obra social, se envia un espacio en blanco
+
     if($obraSocial==="no") {
         $obraSocialTexto = "";
     }
 
+//inserta los datos ingresados
+
     $sql = "INSERT INTO paciente VALUES (NULL,'$nombre','$apellido', '$dni', '$edad', '$telefono', '$sexo', '$direccion', '$localidad', '$provincia', '$obraSocial', '$obraSocialTexto', '$fecha_nacimiento',  '$medicoAsignado')";
     echo ($sql);
+
+//en caso de que la consulta se pueda realizar, dirige hacia otra pagina
 
     if ($conexion->query($sql) === TRUE) {
         header("location:mostrarDatos.php");
     } else {
         echo "Error al guardar la ficha médica: " . $conexion->error;
     }
+ 
+ //cierra la conexion
     
     $conexion->close();
     ?>
