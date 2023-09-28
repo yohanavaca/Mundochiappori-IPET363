84f2af1348d847de5932715fe3ef781e60622156
WAIO21 <132699818+WAIO21@users.noreply.github.com>
2023-09-28 16:27:17 -0300

    Update guardarArea.php

--- olimpiadas-grupo8/guardarArea.php
+++ olimpiadas-grupo8/guardarArea.php
@@ -1,9 +1,16 @@
 <?php
     include("conexion.php");
+    
+//trae el dato del formulario    
+    
     $area = $_POST['area'];
+
+//inserta el dato
 
     $query = "INSERT INTO area (area) VALUES('$area')";
     $resultado= $conexion->query($query);
+
+//si funciona, redirige a la pagina del admin
 
     if($resultado){
         header ("location:administrador.php");
