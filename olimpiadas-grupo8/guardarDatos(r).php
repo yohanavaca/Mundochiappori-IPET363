f2d9d95affe67c5b53946d1697c58e8f2dfc8f79
WAIO21 <132699818+WAIO21@users.noreply.github.com>
2023-09-28 16:27:44 -0300

    Update guardarDatos(r).php

--- olimpiadas-grupo8/guardarDatos(r).php
+++ olimpiadas-grupo8/guardarDatos(r).php
@@ -1,6 +1,7 @@
 <?php
     include("conexion.php");
 
+//trae los datos del formulario
     
     $medicoAsignado = $_POST['medicoAsignado'];
     $diagnostico = $_POST['diagnostico'];
@@ -8,16 +9,20 @@
     $observaciones = $_POST['observaciones'];
     $dni = $_POST['dni'];
 
-    
+//inserta los datos    
 
     $sql = "INSERT INTO reporte VALUES(NULL, '$medicoAsignado', '$diagnostico', '$tratamiento', '$observaciones', '$dni')";
     echo ($sql);
+
+//en el caso que se inserten, dirige a otra pagina
 
     if ($conexion->query($sql) === TRUE) {
         header("Location:pacientesRegistrados.php");
     } else {
         echo "Error al guardar la ficha médica: " . $conexion->error;
     }
+
+//cierra la conexion
     
     $conexion->close();
     ?>
