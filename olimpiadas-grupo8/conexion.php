032208f9ff1a5123a08a62a99fefc929387a606e
WAIO21 <132699818+WAIO21@users.noreply.github.com>
2023-09-28 16:22:28 -0300

    Update conexion.php

--- olimpiadas-grupo8/conexion.php
+++ olimpiadas-grupo8/conexion.php
@@ -1,3 +1,4 @@
 <?php
-$conexion = mysqli_connect("localhost", "root","","datos_paciente")or exit ("no se puede conectar");
+//conecta a la base de datos
+$conexion = mysqli_connect("localhost", "id21316587_val","Aleli1918_","id21316587_datos_paciente")or exit ("no se puede conectar");
 ?>
