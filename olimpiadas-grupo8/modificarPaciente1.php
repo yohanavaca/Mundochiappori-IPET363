9c6f02b0d04d852bbb691f0ec9da5db947784d67
WAIO21 <132699818+WAIO21@users.noreply.github.com>
2023-09-28 16:31:52 -0300

    Update modificarPaciente1.php

--- olimpiadas-grupo8/modificarPaciente1.php
+++ olimpiadas-grupo8/modificarPaciente1.php
@@ -6,10 +6,14 @@
     <title>Document</title>
 </head>
 <body>
+    
+<!--boton de modificar-->
+    
 <form action="consultaPaciente.php" method="post">
         
         <label for="nombre">Modificar:</label>
         <input type="text" id="nombre" name="nombre" required><br><br>
 </form>
+
 </body>
 </html>
