6dc09f72bbdf49bc755bb90aa33f0541864fefbf
WAIO21 <132699818+WAIO21@users.noreply.github.com>
2023-09-28 16:37:27 -0300

    Update validar.php

--- olimpiadas-grupo8/validar.php
+++ olimpiadas-grupo8/validar.php
@@ -1,12 +1,15 @@
 <?php
 
+//inicia la sesion
+
 session_start();
+
+//trae los datos ingresados
 
 $usuario_session = $_POST['usuario'];
 $password_session= $_POST['password'];
 
-
-
+//verifica si los datos ingresados coinciden con los de la base
 
 if($usuario_session=='admin' && $password_session=='espe363'){
   $_SESSION['administrador'] = $usuario_session;
