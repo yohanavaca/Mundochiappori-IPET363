3c35f4422e397d94cea838906d56ac557e4d7649
WAIO21 <132699818+WAIO21@users.noreply.github.com>
2023-09-28 16:25:23 -0300

    Update consultaRegistrarMedico.php

--- olimpiadas-grupo8/consultaRegistrarMedico.php
+++ olimpiadas-grupo8/consultaRegistrarMedico.php
@@ -1,20 +1,27 @@
 <?php
     include("conexion.php");
 
+//trae los datos del formulario
+
     $nombre = $_POST['nombre'];
     $apellido = $_POST['apellido'];
     $telefono = $_POST['telefono'];
     $area = $_POST['area'];
     $dni = $_POST['dni'];
 
+//inserta los datos
+
     $sql = "INSERT INTO medico VALUES(NULL,'$nombre','$apellido', '$telefono', '$area', '$dni')";
     
+//si se pueden ingresar, deriva a la pagina principal
 
     if ($conexion->query($sql) === TRUE) {
         header("location:administrador.php");
     } else {
         echo "Error al guardar la ficha médica: " . $conexion->error;
     }
+
+//cierra la conexion
     
     $conexion->close();
     ?>
